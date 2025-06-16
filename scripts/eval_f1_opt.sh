3d950ff21dd7cfe0430052caf190ac22dbbbb1c7
richardSHkim <shkim@superb-ai.com>
2025-06-16 07:08:43 +0000

    remove unused scripts

--- scripts/eval_f1_opt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-python scripts/eval_f1_opt.py \
-    --gt_json datasets/FishEyeChallenge/FishEye8K/val/val.json \
-    --pred_json results/predictions/yolo11n.json \
-    --save_json results/f1/yolo11n.json
