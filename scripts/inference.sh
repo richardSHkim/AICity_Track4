a09f447cc9cc0b86a514347d89ebcabde1ee20e6
richardSHkim <kshtrue95@gmail.com>
2025-06-18 02:10:14 +0000

    use test image by default

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -2,6 +2,6 @@
 
 
 python scripts/inference.py \
-    --image_folder datasets/FishEyeChallenge/FishEye8K/val/images \
+    --image_folder datasets/FishEyeChallenge/FishEye8K/test/images \
     --model_path checkpoints/yolo11m.pt \
     --save_pred_json results/predictions/yolo11m.json
